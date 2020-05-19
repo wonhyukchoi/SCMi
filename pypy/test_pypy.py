bbacb69d4ee4d44bf480d9b4a66c0ae87e25bab7
Wonhyuk Choi <woc997@g.harvard.edu>
2020-05-19 22:38:49 +0900

    pypy init

---
+++ pypy/test_pypy.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestPyPy(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
